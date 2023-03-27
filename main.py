90fe9f14f314ea1849905fbcd299e56b5a29e15a
fathimasilviyak <silviyakvm@gmail.com>
2023-03-27 16:49:03 +0530

    create individual invitation letter for each name

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+PLACEHOLDER = "[name]"
+# read names from invited names as list
 with open("Input/Names/invited_names.txt") as name_file:
-    name = name_file.readlines()
-    
+    names = name_file.readlines()
+
+# for each name create separate letter with their name
+with open("Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
